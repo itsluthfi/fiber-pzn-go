17493eff28d90339a3479aa60688a7f1ddfb5eea
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-02-17 21:17:44 +0700

    Fiber App

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		panic(err)
+	}
+}
